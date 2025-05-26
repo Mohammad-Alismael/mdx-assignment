b365086f634612caf399ec7feab6cf47f9f014a5
Mohammad-Alismael <mhdhd260@gmail.com>
2025-05-26 17:51:55 +0400

    initial commit

---
+++ src/test/java/com/mdx/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdx.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
